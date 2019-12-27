22a26279fe1cc492e727bd18b6077f8b2dc4dbde
Daniel Lutton <eyedea32@gmail.com>
2019-12-26 19:33:31 -0800

    removing unrelated files to the web build

--- ios/Classes/FlutterTtsPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterTtsPlugin.h"
-#import <flutter_tts/flutter_tts-Swift.h>
-
-@implementation FlutterTtsPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterTtsPlugin registerWithRegistrar:registrar];
-}
-@end
