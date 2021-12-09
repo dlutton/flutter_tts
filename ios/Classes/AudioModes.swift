5d0d7f7644c2e668f99d6fce1292b1e653fb72c9
Milad Taghavi <mitghi@me>
2021-12-09 11:36:37 +0100

    Correct format

--- ios/Classes/AudioModes.swift
+++ ios/Classes/AudioModes.swift
@@ -10,54 +10,54 @@ enum AudioModes: String {
   case iosAudioModeVideoRecording
   case iosAudioModeVoiceChat
   case iosAudioModeVoicePrompt
-  
+
   func toAVAudioSessionMode() -> AVAudioSession.Mode? {
     switch self {
     case .iosAudioModeDefault:
-      if #available(iOS 12.0, *) {        
+      if #available(iOS 12.0, *) {
           return .default
       }
       return nil
     case .iosAudioModeGameChat:
-        if #available(iOS 12.0, *) {                
+        if #available(iOS 12.0, *) {
             return .gameChat
         }
         return nil
     case .iosAudioModeMeasurement:
-        if #available(iOS 12.0, *) {                
+        if #available(iOS 12.0, *) {
             return .measurement
         }
         return nil
     case .iosAudioModeMoviePlayback:
-        if #available(iOS 12.0, *) {        
+        if #available(iOS 12.0, *) {
             return .moviePlayback
         }
         return nil
     case .iosAudioModeSpokenAudio:
-        if #available(iOS 12.0, *) {        
+        if #available(iOS 12.0, *) {
             return .spokenAudio
         }
         return nil
     case .iosAudioModeVideoChat:
-        if #available(iOS 12.0, *) {        
+        if #available(iOS 12.0, *) {
             return .videoChat
         }
         return nil
     case .iosAudioModeVideoRecording:
-        if #available(iOS 12.0, *) {        
+        if #available(iOS 12.0, *) {
             return .videoRecording
         }
         return nil
     case .iosAudioModeVoiceChat:
-        if #available(iOS 12.0, *) {            
+        if #available(iOS 12.0, *) {
             return .voiceChat
         }
         return nil
     case .iosAudioModeVoicePrompt:
-        if #available(iOS 12.0, *) {                
+        if #available(iOS 12.0, *) {
             return .voicePrompt
         }
-        return nil  
+        return nil
     }
   }
 }
