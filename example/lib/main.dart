ca23441b39e134076d601a2ff3e587f1d822360d
sososdk <csshuai2009@gmail.com>
2023-11-30 15:58:55 +0800

    fix(android): when TextToSpeech initialization fails, the dart side method will always be in a pending state .

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:io' show Platform;
-import 'package:flutter/foundation.dart' show kIsWeb;
 
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
@@ -60,14 +60,6 @@ class _MyAppState extends State<MyApp> {
         ttsState = TtsState.playing;
       });
     });
-
-    if (isAndroid) {
-      flutterTts.setInitHandler(() {
-        setState(() {
-          print("TTS Initialized");
-        });
-      });
-    }
 
     flutterTts.setCompletionHandler(() {
       setState(() {
