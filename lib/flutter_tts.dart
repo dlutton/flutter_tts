13d020527cc30923d08fe97c25ee93f4c96db19d
Joseph Crowell <joseph.w.crowell@gmail.com>
2023-12-06 10:07:35 +1000

    The import of 'package:flutter/foundation.dart' is unnecessary because all of the used elements are also provided by the import of 'package:flutter/services.dart'.

--- lib/flutter_tts.dart
+++ lib/flutter_tts.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:io' show Platform;
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 typedef void ErrorHandler(dynamic message);
